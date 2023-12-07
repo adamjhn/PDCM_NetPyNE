d6a66469136dc2e517df04b74c6950885c737949
adam newton <adam.newton@yale.edu>
2023-12-07 13:38:43 -0500

    Use background input instead of recorded input.

--- netParamsSS.py
+++ netParamsSS.py
@@ -22,7 +22,6 @@ except:
     )  # if no simConfig in parent module, import directly from cfg.py:cfg
 
 
-
 # examples of  input/output relation for LIF network model
 data = pickle.load(
     open(
@@ -120,50 +119,179 @@ netParams.synMechParams["inh"] = {
 
 
 ############################################################
-# Connectivity parameters
+# External input parameters
 ############################################################
-def filterTimes(inputs, weights):
-    inp = [inputs[0]]
-    wei = [weights[0]]
-    for t, w in zip(inputs[1:], weights[1:]):
-        if t - inp[-1] - 2 > 1e-9:
-            inp.append(t - 2)
-            wei.append(w)
-        else:
-            wei[-1] += w
-    return (inp, wei)
+# Reescaling function (move to separate module?)
+# Reescaling function (move to separate module?)
+def Reescale(ScaleFactor, C, N_Full, w_p, f_ext, tau_syn, Inp, InpDC):
+    if ScaleFactor < 1.0:
+        # This is a good approximation of the F_out param for the Balanced option "True".
+        # Note for the Balanced=False option, it should be possible to calculate a better approximation.
+        F_out = np.array([0.860, 2.600, 4.306, 5.396, 8.142, 8.188, 0.941, 7.3])
+
+        Ncon = np.vstack(
+            [np.column_stack([0 for i in range(0, 8)]) for i in range(0, 8)]
+        )
+        for r in range(0, 8):
+            for c in range(0, 8):
+                Ncon[r][c] = (
+                    np.log(1.0 - C[r][c]) / np.log(1.0 - 1.0 / (N_Full[r] * N_Full[c]))
+                ) / N_Full[r]
+
+        w = w_p * np.column_stack(
+            [np.vstack([[1.0, -4.0] for i in range(0, 8)]) for i in range(0, 4)]
+        )
+        w[0][2] = 2.0 * w[0][2]
+
+        x1_all = w * Ncon * F_out
+        x1_sum = np.sum(x1_all, axis=1)
+        x1_ext = w_p * Inp * f_ext
+        I_ext = np.column_stack([0 for i in range(0, 8)])
+        I_ext = (
+            0.001
+            * tau_syn
+            * (
+                (1.0 - np.sqrt(ScaleFactor)) * x1_sum
+                + (1.0 - np.sqrt(ScaleFactor)) * x1_ext
+            )
+        )
+
+        InpDC = np.sqrt(ScaleFactor) * InpDC * w_p * f_ext * tau_syn * 0.001  # pA
+        w_p = w_p / np.sqrt(ScaleFactor)  # pA
+        InpDC = InpDC + I_ext
+        N_ = [int(ScaleFactor * N) for N in N_Full]
+    else:
+        InpDC = InpDC * w_p * f_ext * tau_syn * 0.001
+        N_ = N_Full
+
+    return InpDC, N_, w_p
 
 
-# cells are disconnect and just receive pre-recorded network activity
-for pop, sz in zip(L, N_Full):
-    for idx in range(sz):
-        inp = data[pop]["inputs"][idx]
-        wei = data[pop]["weight"][idx]
-        exc = wei >= 0
-        inh = wei < 0
-        # sum inputs less than 10^-12s apart
-        for syn in ["exc", "inh"]:
-            if syn == "exc":
-                inputs, weights = filterTimes(inp[exc], wei[exc])
-            else:
-                inputs, weights = filterTimes(inp[inh], wei[inh])
+# Frequency of external input
+f_ext = 8.0  # (Hz)
+# Postsynaptic current time constant
+tau_syn = 0.5  # (ms)
+# Membrane time constant
+tau_m = 10  # (ms)
+# Other constants defined inside .mod
+"""
+#Absolute refractory period
+tauref =2 (ms) 
+#Reset potential
+Vreset = -65 (mV) : -49 (mV) :
+#Fixed firing threshold
+Vteta  = -50 (mV)"""
+# Membrane capacity
+C_m = cfg.Cm / (2e-8 * np.pi * cfg.somaR**2)  # pF
+# Mean amplitude of the postsynaptic potential (in mV).
+w_v = 0.15
+# Mean amplitude of the postsynaptic potential (in pA).
+w_p = (
+    (C_m) ** (-1)
+    * tau_m
+    * tau_syn
+    / (tau_syn - tau_m)
+    * (
+        (tau_m / tau_syn) ** (-tau_m / (tau_m - tau_syn))
+        - (tau_m / tau_syn) ** (-tau_syn / (tau_m - tau_syn))
+    )
+) ** (-1)
+w_p = w_v * w_p  # (pA)
 
-            # create a source from the pre-recorded spikes
-            netParams.popParams[f"{pop}_{idx}_{syn}"] = {
-                "cellModel": "VecStim",
-                "type": "VecStim",
-                "spkTimes": inputs,
-                "weights": weights,
-                "numCells": 1,
-            }
-            # play the source into the target cell
-            netParams.connParams[f"{pop}_{idx}_{syn}"] = {
-                "preConds": {"pop": f"{pop}_{idx}_{syn}"},
-                "postConds": {"pop": f"{pop}_{idx}"},
-                "weight": cfg.excWeight if syn == "exc" else cfg.inhWeight,
-                "delay": 2,
-                "synMech": syn,
-            }
+# C probability of connection
+C = np.array(
+    [
+        [0.1009, 0.1689, 0.0437, 0.0818, 0.0323, 0.0, 0.0076, 0.0],
+        [0.1346, 0.1371, 0.0316, 0.0515, 0.0755, 0.0, 0.0042, 0.0],
+        [0.0077, 0.0059, 0.0497, 0.135, 0.0067, 0.0003, 0.0453, 0.0],
+        [0.0691, 0.0029, 0.0794, 0.1597, 0.0033, 0.0, 0.1057, 0.0],
+        [0.1004, 0.0622, 0.0505, 0.0057, 0.0831, 0.3726, 0.0204, 0.0],
+        [0.0548, 0.0269, 0.0257, 0.0022, 0.06, 0.3158, 0.0086, 0.0],
+        [0.0156, 0.0066, 0.0211, 0.0166, 0.0572, 0.0197, 0.0396, 0.2252],
+        [0.0364, 0.001, 0.0034, 0.0005, 0.0277, 0.008, 0.0658, 0.1443],
+    ]
+)
+
+# Population size N
+L = ["L2e", "L2i", "L4e", "L4i", "L5e", "L5i", "L6e", "L6i"]
+N_Full = np.array([20683, 5834, 21915, 5479, 4850, 1065, 14395, 2948, 902])
+
+# Number of Input per Layer
+Inp = np.array([1600, 1500, 2100, 1900, 2000, 1900, 2900, 2100])
+if cfg.Balanced == False:
+    InpUnb = np.array([2000, 1850, 2000, 1850, 2000, 1850, 2000, 1850])
+###########################################################
+# Reescaling calculation
+###########################################################
+if cfg.DC == True:
+    InpDC = Inp
+    if cfg.Balanced == False:
+        InpDC = InpUnb
+else:
+    InpDC = np.zeros(8)
+    InpPoiss = Inp * cfg.ScaleFactor
+    if cfg.Balanced == False:
+        InpPoiss = InpUnb * cfg.ScaleFactor
+
+InpDC, N_, w_p = Reescale(cfg.ScaleFactor, C, N_Full, w_p, f_ext, tau_syn, Inp, InpDC)
+print("Inputs", InpDC, N_, w_p)
+
+
+netParams.delayMin_e = 1.5
+netParams.ddelay = 0.5
+netParams.delayMin_i = 0.75
+netParams.weightMin = w_p
+netParams.dweight = 0.1
+
+if cfg.DC == False:  # External Input as Poisson
+    for r in range(0, 8):
+        netParams.popParams["poiss" + str(L[r])] = {
+            "numCells": 1,
+            "cellModel": "NetStim",
+            "rate": InpPoiss[r] * f_ext,
+            "start": 0.0,
+            "noise": 1.0,
+            "delay": 0,
+        }
+
+        auxConn = np.array([range(0, N_[r], 1), range(0, N_[r], 1)])
+        for idx in range(10):
+            netParams.connParams[f"poiss->{L[r]}_{idx}"] = {
+                "preConds": {"pop": "poiss" + str(L[r])},
+                "postConds": {"pop": f"{L[r]}_{idx}"},
+                #"connList": auxConn.T,
+                "weight": f"max(0, {cfg.excWeight} * (weightMin+normal(0,dweight*weightMin)))",
+                "delay": 0.5,
+                "synMech" : "exc",
+            }  # 1 delay
+
+# Thalamus Input: increased of 15Hz that lasts 10 ms
+# 0.15 fires in 10 ms each 902 cells -> number of spikes = T*f*N_ = 0.15*902 -> 1 spike each N_*0.15
+if cfg.TH == True:
+    fth = 15  # Hz
+    Tth = 10  # ms
+    InTH = [0, 0, 93, 84, 0, 0, 47, 34]
+    for r in [2, 3, 6, 7]:
+        nTH = int(np.sqrt(cfg.ScaleFactor) * InTH[r] * fth * Tth / 1000)
+        netParams.popParams["bkg_TH" + str(L[r])] = {
+            "numCells": 1,
+            "cellModel": "NetStim",
+            "rate": 2 * (1000 * nTH) / Tth,
+            "start": 200.0,
+            "noise": 1.0,
+            "number": nTH,
+            "delay": 0,
+        }
+        auxConn = np.array([range(0, N_[r], 1), range(0, N_[r], 1)])
+        for idx in range(10):
+            netParams.connParams[f"bkg_TH->{L[r]}_{idx}"] = {
+                "preConds": {"pop": "bkg_TH" + str(L[r])},
+                "postConds": {"pop": f"{L[r]}_{idx}"},
+                #"connList": auxConn.T,
+                "weight": f"max(0, {cfg.excWeight} * (weightMin +normal(0,dweight*weightMin)))",
+                "synMech": "exc",
+                "delay": 0.5,
+            }  # 1 delay
 
 
 ############################################################
@@ -191,7 +319,7 @@ constants = {
     "g_gliamax": 5 * mM / sec,
     "beta0": 7.0,
     "avo": 6.0221409 * (10**23),
-    "p_max": cfg.pmax * mM/sec,
+    "p_max": cfg.pmax * mM / sec,
     "nao_initial": 144.0,
     "nai_initial": 18.0,
     "gnai_initial": 18.0,
@@ -201,7 +329,7 @@ constants = {
     "clo_initial": 130.0,
     "cli_initial": 6.0,
     "o2_bath": cfg.o2_bath,
-    "v_initial": -70.0,
+    "v_initial": cfg.hParams["v_init"],
 }
 
 # sodium activation 'm'
@@ -517,7 +645,7 @@ kbalance = f"-(({nkcc1} + {kcc2} - 2 * {pump}) * {volume_scale} + ({gk} * (v_ini
 nabalance = f"-(({nkcc1} + 3 * {pump}) * {volume_scale} + ({gna} * (v_initial - {ena})))/(v_initial-{ena})"
 
 constants["gclbar_l"] = initEval(clbalance)
-constants["gkbar_l"] = initEval(kbalance)
+constants["gkbar_l"] = cfg.gkleak_scale * initEval(kbalance)
 constants["gnabar_l"] = initEval(nabalance)
 
 
