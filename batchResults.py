045e5c69d138b560acff56633345bb5051bab010
Adam Newton <adam.newton@yale.edu>
2025-10-15 16:52:26 -0400

    Update single cell parameters for lower rxdscore (0.4389813620299378)

--- batchResults.py
+++ batchResults.py
@@ -1,8 +1,35 @@
 import pandas as pd
 from sqlite3 import connect
+import json
+import numpy as np
 
-simLabel = "cellFit3" #"weightsRate"
-conn = connect(f'/data/adam/{simLabel}/{simLabel}_storage.db')
+simLabel = "cellFit4" #"weightsRate"
+savepath = f"/data/adam/{simLabel}"
+conn = connect(f'{savepath}/{simLabel}_storage.db')
+maxFitness = 1_000_000
+
+
+taum = 10  # ms
+tauref = 2  # ms
+Vrest = -65  # mV
+Vth = -50  # mV
+Cm = 250  # pF
+R = taum / (1e-3 * Cm)  # Mohms
+Vreset = -65
+Vth = -50
+
+# caclulate target freq
+amps = np.linspace(0.07, 0.7, 10) #netParams.amps
+target = [
+    1e3 / (taum * np.log(R * a / (Vreset - Vth + R * a)) + tauref)
+    if Vreset - Vth + R * a > 0
+    else 0
+    for a in amps
+]
+rheobase = (Vth - Vreset)/R
+
+
+
 
 def batch_params():
     params = dict()
@@ -39,3 +66,58 @@ GROUP BY
 
 
 df = pd.read_sql(query, conn)
+
+
+def fitnessFunc(sd, **kwargs):
+    if len(sd["spkid"]) > 0:
+        spkid = np.array(sd["spkid"], dtype=int)
+        freq = np.array([sum(spkid == i) for i, _ in enumerate(amps)])
+        # range 0-1 unless freq> 2 * target[-1]
+        freqscore = sum(abs(target - freq))/len(amps)/target[-1]
+         # range 0-1
+        rheobaseScore = abs(rheobase-amps[spkid.min()])/(amps[-1]-rheobase)
+    else:
+        freqscore = 1.0
+        rheobaseScore = 1.0
+    rxdscore, o2score = 0, 0
+    for gid, _ in enumerate(amps):
+        for ion in ["k", "na", "cl"]:
+            trace = sd[f"{ion}i_soma"][f"cell_{gid}"]
+            rxdscore += abs(trace[0] - trace[-1]) / trace[0]
+            o2score += sd["o2_consumedo_soma"][f"cell_{gid}"][
+                -1
+            ]  # amount of oxygen consumed
+    return freqscore, rheobaseScore, rxdscore, o2score
+
+
+
+freqscores, rheobaseScores, rxdscores, o2scores = [], [], [], [] 
+for num in df['number']:
+    try:
+        data = json.load(open(f"{savepath}/gen_{num}/trial_{num}_data.json","r"))
+    except FileNotFoundError:
+        freqscores.append(maxFitness)
+        rheobaseScores.append(maxFitness)
+        rxdscores.append(maxFitness)
+        o2scores.append(maxFitness)
+        continue
+    sd = data['simData']
+    f, r, rx, ox = fitnessFunc(sd)
+    freqscores.append(f)
+    rheobaseScores.append(r)
+    rxdscores.append(rx)
+    o2scores.append(ox)
+df['freqScore'] = freqscores
+df['rheobaseScore'] = rheobaseScores
+df['rxdscore'] = rxdscores
+df['o2score'] = o2scores
+
+
+# filter for reasonable (spiking) results
+df = df[df['trial_value']<2].sort_values('trial_value')
+idx = df['rxdscore'].argin()
+
+# print cfg (and results)
+for k,v in df.iloc[idx].items():
+    print(f"cfg.{k} = {v}")
+
