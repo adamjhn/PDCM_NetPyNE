d177addb7a01f844eb819236e125b05e302148eb
Adam Newton <adam.newton@yale.edu>
2025-10-28 14:06:12 -0400

    Add VClamp to hold neurons at RMP for the first 25ms to allow synaptic states to rise.

--- initSSVecStim.py
+++ initSSVecStim.py
@@ -5,10 +5,10 @@ init.py -- code to run the simulation
 """
 
 from netpyne import sim
-from neuron import h
+from neuron import h, rxd
 import pandas as pd
 import numpy as np
-
+import pickle
 
 ############################################################
 #               Create network and run simulation
@@ -48,6 +48,18 @@ sim.net.createCells()  # instantiate network cells based on defined populations
 sim.net.addStims()  # add network stimulation
 # fih = h.FInitializeHandler(2, lambda: fi(sim.net.cells))
 sim.net.addRxD(nthreads=6)
+
+clamps = []
+for cell in sim.net.cells:
+    if cell.tags['cellModel'] != "VecStim":
+        vclamp = h.VClamp(cell.secs['soma']['hObj'](0.5))
+        vclamp.dur[0] = 25
+        vclamp.dur[1] = 0
+        vclamp.dur[2] = 0
+        vclamp.amp[0] = -70
+        clamps.append(vclamp)
+
+
 """
 df = pd.read_json('PDMCExample.json')
 
@@ -66,11 +78,32 @@ for gid in range(N_Full).sum()):
 """
 sim.net.connectCells()  # create connections between cells based on params
 sim.setupRecording()  # setup variables to record for each cell (spikes, V traces, etc)
+
+# extra recording
+"""
+for sp in rxd.species._all_defined_species:
+    if sp().name == 'mgate':
+        mgate = sp()
+    elif sp().name == 'hgate':
+        hgate = sp()
+    elif sp().name == 'ngate':
+        ngate = sp()
+extraRec = {}
+for cellName in sim.cfg.recordCells:
+    dat = {}
+    cell = sim.getCellsList(include=[cellName])[0]
+    dat['mgate'] = h.Vector().record(mgate.nodes(cell.secs['soma']['hObj'])._ref_value, sim.cfg.recordStep)
+    dat['hgate'] = h.Vector().record(hgate.nodes(cell.secs['soma']['hObj'])._ref_value, sim.cfg.recordStep)
+    dat['ngate'] = h.Vector().record(ngate.nodes(cell.secs['soma']['hObj'])._ref_value, sim.cfg.recordStep)
+    extraRec[cellName] = dat
+""" 
+
 sim.runSim()  # run parallel Neuron simulation
 sim.gatherData()  # gather spiking data and cell info from each node
 sim.saveData()  # save params, cell info and sim output to file (pickle,mat,txt,etc)#
 # sim.analysis.plotData()               # plot spike raster etc
 
+pickle.dump(extraRec, open(f"{sim.cfg.saveFolder}/extraRec.pkl",'wb'))
 
 # # Plot all electrodes separately; use electrode 6
 # for elec in [3]: #range(15):
